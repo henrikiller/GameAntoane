f5d9badb5ef446ff4353bbbbd4cc90addeb8579f
Luiz Henrique Sacramento <LuizSacramento.nave@gmail.com>
2015-03-11 11:15:41 -0300

    Lista Duplamente Encadeada

---
+++ LuizHenrique_Ingrid_Ricardo_LuizFernando/2015-3003-1BIM-ListaEncadeada/Lista.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace _2015_3003_1BIM_ListaEncadeada
+{
+    class Lista
+    {
+        private Elemento primeiro;
+        public Elemento Primeiro
+        {
+            get
+            {
+                return primeiro;
+            }
+            set
+            {
+                primeiro = value;
+            }
+        }
+
+        public Elemento Ultimo
+        {
+            get
+            {
+                return BuscaUltimo();
+            }
+        }
+
+        public Lista()
+        {
+            
+        }
+
+        public Elemento BuscaEmLocal(int n)
+        {
+            Elemento current = Primeiro;
+            int count = 1;
+
+            while(count < n)
+            {
+                current = current.Proximo;
+                count++;
+            }
+
+            return current;
+        }
+
+        public void Adiciona(Elemento e)
+        {
+            if (Primeiro == null)
+            {
+                Primeiro = e;
+            }
+            else
+            {
+                Ultimo.Proximo = e;
+            }
+        }
+
+        public Elemento BuscaUltimo()
+        {
+            Elemento current = Primeiro;
+            while (current.Proximo != null)
+            {
+                current = current.Proximo;
+            }
+            return current;
+        }
+
+        public int Count
+        {
+            get {
+                int count = 1;
+                Elemento current = Primeiro;
+                while (current != null)
+                {
+                    current = current.Proximo;
+                    count++;
+                }
+                return count;
+            }
+        }
+
+        public void ImprimeLista()
+        {
+            Elemento current = Primeiro;
+            while (current != null)
+            {
+                Debug.WriteLine(current.AsString);
+                current = current.Proximo;
+            } 
+        }
+    }
+}
